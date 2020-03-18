b392148cfd96852e4c2ab4cfe041b91f472c08d3
doatridge <darryl@opengrass.io>
2020-03-17 22:46:30 -0500

    - update event_book_portfolio.py with removed params

--- ds_engines/components/event_book_portfolio.py
+++ ds_engines/components/event_book_portfolio.py
@@ -32,8 +32,7 @@ class EventBookPortfolio(AbstractComponent):
     @classmethod
     def from_uri(cls, task_name: str, uri_pm_path: str, pm_file_type: str=None, pm_module: str=None,
                  pm_handler: str=None, pm_kwargs: dict=None, default_save=None, reset_templates: bool=None,
-                 align_connectors: bool=None, default_save_intent: bool=None, default_intent_level: bool=None,
-                 order_next_available: bool=None, default_replace_intent: bool=None):
+                 align_connectors: bool=None, default_save_intent: bool=None, **kwargs):
         """ Class Factory Method to instantiates the components application. The Factory Method handles the
         instantiation of the Properties Manager, the Intent Model and the persistence of the uploaded properties.
         See class inline docs for an example method
@@ -49,23 +48,44 @@ class EventBookPortfolio(AbstractComponent):
                                 (see `report_environ()`)
          :param align_connectors: (optional) resets aligned connectors to the template. default Default True
          :param default_save_intent: (optional) The default action for saving intent in the property manager
-         :param default_intent_level: (optional) the default level intent should be saved at
-         :param order_next_available: (optional) if the default behaviour for the order should be next available order
-         :param default_replace_intent: (optional) the default replace existing intent behaviour
          :return: the initialised class instance
          """
         pm_file_type = pm_file_type if isinstance(pm_file_type, str) else 'pickle'
         pm_module = pm_module if isinstance(pm_module, str) else 'ds_discovery.handlers.pandas_handlers'
         pm_handler = pm_handler if isinstance(pm_handler, str) else 'PandasPersistHandler'
         _pm = EventBookPropertyManager(task_name=task_name)
-        _intent_model = EventBookIntentModel(property_manager=_pm, default_save_intent=default_save_intent,
-                                             default_intent_level=default_intent_level,
-                                             order_next_available=order_next_available,
-                                             default_replace_intent=default_replace_intent)
+        _intent_model = EventBookIntentModel(property_manager=_pm, default_save_intent=default_save_intent)
         super()._init_properties(property_manager=_pm, uri_pm_path=uri_pm_path, pm_file_type=pm_file_type,
                                  pm_module=pm_module, pm_handler=pm_handler, pm_kwargs=pm_kwargs)
         return cls(property_manager=_pm, intent_model=_intent_model, default_save=default_save,
                    reset_templates=reset_templates, align_connectors=align_connectors)
+
+    @classmethod
+    def from_env(cls, task_name: str, default_save=None, reset_templates: bool=None, align_connectors: bool=None,
+                 default_save_intent: bool=None, **kwargs):
+        """ Class Factory Method that builds the connector handlers taking the property contract path from
+        the os.environ['AISTAC_PM_PATH'] or, if not found, uses the system default,
+                    for Linux and IOS '/tmp/components/contracts
+                    for Windows 'os.environ['AppData']\\components\\contracts'
+        The following environment variables can be set:
+        'AISTAC_PM_PATH': the property contract path, if not found, uses the system default
+        'AISTAC_PM_TYPE': a file type for the property manager. If not found sets as 'pickle'
+        'AISTAC_PM_MODULE': a default module package, if not set uses component default
+        'AISTAC_PM_HANDLER': a default handler. if not set uses component default
+
+        This method calls to the Factory Method 'from_uri(...)' returning the initialised class instance
+
+         :param task_name: The reference name that uniquely identifies a task or subset of the property manager
+         :param default_save: (optional) if the configuration should be persisted
+         :param reset_templates: (optional) reset connector templates from environ variables. Default True
+                                (see `report_environ()`)
+         :param align_connectors: (optional) resets aligned connectors to the template. default Default True
+         :param default_save_intent: (optional) The default action for saving intent in the property manager
+         :param kwargs: to pass to the property ConnectorContract as its kwargs
+         :return: the initialised class instance
+         """
+        return super().from_env(task_name=task_name,  default_save=default_save, reset_templates=reset_templates,
+                                align_connectors=align_connectors, default_save_intent=default_save_intent, **kwargs)
 
     @classmethod
     def _from_remote_s3(cls) -> (str, str):
