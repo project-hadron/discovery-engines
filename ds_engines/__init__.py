0fe26c0bf65be64e1a158148a7a9a3ae5030fa98
Gigas64 <darryl@aistac.net>
2020-06-08 11:29:36 -0500

    - moved EventBookPortfolio to the upper level for import

--- ds_engines/__init__.py
+++ ds_engines/__init__.py
@@ -1,4 +1,5 @@
 # bring definitions to the top level
+from ds_engines.components.event_book_portfolio import EventBookPortfolio
 
 # release version number picked up in the setup.py
-__version__ = "2.01.052"
+__version__ = "2.01.053"
